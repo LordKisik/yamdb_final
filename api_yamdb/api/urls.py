e55c2c0d9f0dbeecb4369324ae4cc88429e7b0ea
Виталий Никонов <nikonov666vitaly@mail.ru>
2023-04-11 02:45:34 +0300

    2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (UserViewSet, SignupView, GetTokenView, CategoryViewSet,
-                    GenreViewSet, TitleViewSet, ReviewViewSet, CommentViewSet)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    GetTokenView, ReviewViewSet, SignupView, TitleViewSet,
+                    UserViewSet)
 
 app_name = 'api'
 
